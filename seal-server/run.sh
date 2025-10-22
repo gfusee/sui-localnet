a7067346c2a91f51c1719c9901f4584cae21c300
quentin <quentus123@gmail.com>
2025-10-22 17:22:47 +0200

    fixes

--- seal-server/run.sh
+++ seal-server/run.sh
@@ -11,11 +11,17 @@ to_vector() {
     echo "vector[${parts[*]}]" | sed 's/ /, /g'
 }
 
-./wait_for_localnet.sh http://localnet:9000
+NODE_URL="${NODE_URL:-http://localnet:9000}"
+echo "Using node URL: $NODE_URL"
+
+FAUCET_URL="${FAUCET_URL:-http://localnet:9123}"
+echo "Using faucet URL: $FAUCET_URL"
+
+./wait_for_localnet.sh $NODE_URL
 
 sui client new-address ed25519
-sui client new-env --alias localnet --rpc http://localnet:9000
-./faucet.sh "$(sui client active-address)"
+sui client new-env --alias localnet --rpc $NODE_URL
+./faucet.sh "$FAUCET_URL" "$(sui client active-address)"
 
 SEAL_PACKAGE_ID="$(./deploy_seal.sh)"
 
@@ -32,11 +38,11 @@ MASTER_KEY=$(echo "$OUTPUT" | grep -oP '(?<=Master key:\s)0x[0-9a-fA-F]+')
 PUBLIC_KEY=$(echo "$OUTPUT" | grep -oP '(?<=Public key:\s)0x[0-9a-fA-F]+')
 
 # Use SEAL_SERVER_URL if defined, otherwise default to localhost:9005
-URL="${SEAL_SERVER_URL:-http://localhost:9005}"
-echo "Using Seal server URL: $URL"
+SEAL_SERVER_URL="${SEAL_SERVER_URL:-http://localhost:9005}"
+echo "Using Seal server URL: $SEAL_SERVER_URL"
 
 KEY_SERVER_JSON="$(sui client ptb \
-  --move-call $SEAL_PACKAGE_ID::key_server::create_and_transfer_v1 '"FirstServer"' "\"$URL\"" 0 $(to_vector "$PUBLIC_KEY") \
+  --move-call $SEAL_PACKAGE_ID::key_server::create_and_transfer_v1 '"FirstServer"' "\"$SEAL_SERVER_URL\"" 0 $(to_vector "$PUBLIC_KEY") \
   --json
 )"
 
@@ -51,5 +57,17 @@ echo "Seal package ID: $SEAL_PACKAGE_ID"
 echo "Key server ID: $KEY_SERVER_OBJECT_ID"
 echo "Master key: $MASTER_KEY"
 
+# Ensure the /shared directory exists
+mkdir -p /shared
+
+# Write the variables into /shared/seal.json
+cat <<EOF > /shared/seal.json
+{
+  "seal_package_id": "$SEAL_PACKAGE_ID",
+  "key_server_object_id": "$KEY_SERVER_OBJECT_ID",
+  "public_key": "$PUBLIC_KEY"
+}
+EOF
+
 # Run the key-server command
-MASTER_KEY="$MASTER_KEY" NETWORK="custom" NODE_URL="http://localnet:9000" KEY_SERVER_OBJECT_ID="$KEY_SERVER_OBJECT_ID" key-server
+MASTER_KEY="$MASTER_KEY" NETWORK="custom" NODE_URL="$NODE_URL" KEY_SERVER_OBJECT_ID="$KEY_SERVER_OBJECT_ID" key-server
