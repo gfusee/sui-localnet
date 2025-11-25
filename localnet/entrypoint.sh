1d005133634a9adc30c440e8edae58ad4af7dad8
quentin <quentus123@gmail.com>
2025-11-25 19:56:02 +0100

    fixes

--- localnet/entrypoint.sh
+++ localnet/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -n "$(ls -A "$dir")" ]; then
+if [ -n "$(ls -A "/localnet-config")" ]; then
     CMD="sui start --with-faucet --network.config=/Users/quentin/WebstormProjects/sui-poker/localnet-config"
 
     if [ -n "$WITH_GRAPHQL" ]; then
