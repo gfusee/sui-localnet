ec50077507a17633906cb7f4ad573f730674da41
quentin <quentus123@gmail.com>
2025-11-25 19:32:31 +0100

    add flags and persistence

---
+++ localnet/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -d "/localnet-config" ]; then
+    CMD="sui start --with-faucet --network.config=/Users/quentin/WebstormProjects/sui-poker/localnet-config"
+
+    if [ -n "$WITH_GRAPHQL" ]; then
+        CMD="$CMD --with-graphql"
+    fi
+
+    if [ -n "$WITH_INDEXER" ]; then
+        CMD="$CMD --with-indexer"
+    fi
+
+    exec sh -c "$CMD"
+else
+    exec sui start --with-faucet --force-regenesis
+fi
